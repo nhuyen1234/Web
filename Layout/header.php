540dfe24ce2eeae784b5b6928f2c793efdfc7ff7
nhuyen1234 <yennnph34437@fpt.edu.vn>
2023-11-29 23:58:15 +0700

    update

--- Layout/header.php
+++ Layout/header.php
@@ -48,7 +48,7 @@
                             <a href="#">Collection</a><!--Collection -->
                             <ul class="sub-menu">
                                 <li><a href="view/thucdon_2.php?id_sanpham=1">One piece</a></li>
-                                <li><a href="view/thucdon_2.php?id_sanpham=2">Spring of the Y</a></li>
+                                <li><a href="view/thucdon_2.php?id_sanpham=2">Spring of the ¥</a></li>
                                 <li><a href="view/thucdon_2.php?id_sanpham=3">Liliwyun</a></li>
                             </ul>
                         </li>
