9cfd1ea280218c85e015bac4ef562d0aaf6185cf
nhuyen1234 <yennnph34437@fpt.edu.vn>
2023-12-06 18:24:28 +0700

    oceee

--- AboutUs/Dirtycoins/Dirtycoins.php
+++ AboutUs/Dirtycoins/Dirtycoins.php
@@ -324,8 +324,8 @@
         <h4>THÔNG TIN LIÊN HỆ</h4><!--Thông tin liên hệ -->
         <p>Tuyển dụng:<a href ="https://www.facebook.com/DirtyCoins.VN/"> https://www.facebook.com/DirtyCoins.VN/</a> </p>
         <p>Website:<a href ="https://dirtycoins.vn/"> https://dirtycoins.vn/</a></p>
-        <p>Liên hệ CSKH: support@<a href ="https://dirtycoins.vn/lien-he"> https://dirtycoins.vn/lien-he</a></p>
-        <p>For business: contact@<a href ="0933 800 190 - 1900252557"> 0933 800 190 - 1900252557</a></p>
+        <p>Liên hệ CSKH: Support@<a href ="https://dirtycoins.vn/lien-he"> https://dirtycoins.vn/lien-he</a></p>
+        <p>For business: Contact@<a href ="0933 800 190 - 1900252557"> 0933 800 190 - 1900252557</a></p>
     </div>
     <div class="col">
         <h4>FOLLOW US ON SOCIAL MEDIA</h4><!--Follow us on social media-->
